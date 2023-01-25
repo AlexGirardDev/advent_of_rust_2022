f2d651d8a8674929138e48d1ae36f48ab304db23
Alex Girard <me@alexgirard.ca>
2023-01-24 21:46:08 -0500

    day 3

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,126 @@
 use std::fs::File;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+const INPUT: &str = include_str!("./input.txt");
+
+
+fn part_1() {
+    let mut score: i32 = 0;
+
+    for line in INPUT.lines() {
+        let your_hand_str = &line[..1];
+        let there_hand_str = &line[2..];
+        let there_hand_val = match your_hand_str {
+            "A" => Hand::Rock,
+            "B" => Hand::Paper,
+            "C" => Hand::Scissors,
+            _ => panic!("woops"),
+        };
+        let plan = Plan {
+            your_hand: match there_hand_str {
+                "X" => match there_hand_val {
+                    Hand::Rock => Hand::Scissors,
+                    Hand::Paper => Hand::Rock,
+                    Hand::Scissors => Hand::Paper,
+                },
+                "Y" => match there_hand_val {
+                    Hand::Rock => Hand::Rock,
+                    Hand::Paper => Hand::Paper,
+                    Hand::Scissors => Hand::Scissors,
+                },
+
+                "Z" => match there_hand_val {
+                    Hand::Rock => Hand::Paper,
+                    Hand::Paper => Hand::Scissors,
+                    Hand::Scissors => Hand::Rock,
+                },
+                _ => panic!("woops"),
+            },
+            there_hand: there_hand_val,
+        };
+
+        score += match plan.your_hand {
+            Hand::Rock => {
+                let bonus = 1;
+                match plan.there_hand {
+                    Hand::Rock => 3 + bonus,
+                    Hand::Paper => 0 + bonus,
+                    Hand::Scissors => 6 + bonus,
+                }
+            }
+            Hand::Paper => {
+                let bonus = 2;
+                match plan.there_hand {
+                    Hand::Rock => 6 + bonus,
+                    Hand::Paper => 3 + bonus,
+                    Hand::Scissors => 0 + bonus,
+                }
+            }
+            Hand::Scissors => {
+                let bonus = 3;
+                match plan.there_hand {
+                    Hand::Rock => 0 + bonus,
+                    Hand::Paper => 6 + bonus,
+                    Hand::Scissors => 3 + bonus,
+                }
+            }
+        };
+    }
+    println!("day2_2 {}", score);
+}
+ fn part_2() {
+    let mut score: i32 = 0;
+
+    for line in INPUT.lines() {
+        let your_hand_str = &line[..1];
+        let there_hand_str = &line[2..];
+        let plan = Plan {
+            there_hand: match your_hand_str {
+                "A" => Hand::Rock,
+                "B" => Hand::Paper,
+                "C" => Hand::Scissors,
+                _ => panic!("woops"),
+            },
+
+            your_hand: match there_hand_str {
+                "X" => Hand::Rock,
+                "Y" => Hand::Paper,
+                "Z" => Hand::Scissors,
+                _ => panic!("woops"),
+            },
+        };
+
+        score += match plan.your_hand {
+            Hand::Rock => {
+                let bonus = 1;
+                match plan.there_hand {
+                    Hand::Rock => 3 + bonus,
+                    Hand::Paper => 0 + bonus,
+                    Hand::Scissors => 6 + bonus,
+                }
+            }
+            Hand::Paper => {
+                let bonus = 2;
+                match plan.there_hand {
+                    Hand::Rock => 6 + bonus,
+                    Hand::Paper => 3 + bonus,
+                    Hand::Scissors => 0 + bonus,
+                }
+            }
+            Hand::Scissors => {
+                let bonus = 3;
+                match plan.there_hand {
+                    Hand::Rock => 0 + bonus,
+                    Hand::Paper => 6 + bonus,
+                    Hand::Scissors => 3 + bonus,
+                }
+            }
+        };
+    }
+    println!("day2_1 {}", score);
+}
 use std::io::{self, BufRead};
 use std::path::Path;
 
