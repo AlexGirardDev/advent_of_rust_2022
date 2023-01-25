42eb0b792a7294d46527ab811e98769b6dbabe92
Alex Girard <me@alexgirard.ca>
2023-01-25 12:41:19 -0500

    day4

---
+++ template/src/main.rs
@@ -0,0 +1,14 @@
+const INPUT: &str = include_str!("./input.txt");
+const TEST: &str = include_str!("./test.txt");
+
+fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
+}
+
+fn part_2() {
+}
+
